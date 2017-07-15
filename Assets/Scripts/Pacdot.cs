7c201e864ebb9519a0476e53a0472bf4216874a8
Unicnight <Unicnight@hotmail.com>
2017-07-15 23:37:15 +0800

    添加项目文件。

---
+++ Assets/Scripts/Pacdot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pacdot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//触发器检测,销毁豆子
+	void OnTriggerEnter2D(Collider2D co)
+	{
+		print ("asd");
+		if (co.name == "pacman")
+			Destroy (gameObject);
+	}
+}
